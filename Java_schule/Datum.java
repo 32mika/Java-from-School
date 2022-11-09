bd001bebe874439685835ee9af3c368beac08736
32mika <117844540+32mika@users.noreply.github.com>
2022-11-09 17:11:32 +0100

    Update Datum.java

--- Java_schule/Datum.java
+++ Java_schule/Datum.java
@@ -1,39 +1,72 @@
-package Java_schule;
+package java_schule;
+
 import java.util.Scanner;
 
-public class Datum {
+public class Datum{
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int tag_h;
-		int monat_h;
-		int jahr_h;
-		int tag_g;
-		int monat_g;
-		int jahr_g;
+		// TODO Auto-generated method stub
+
+		int Tag_heute;
+		int Monat_heute;
+		int Jahr_heute;
+		int Geburtstag;
+		int Geburtsmonat;
+		int Geburtsjahr;
 		int alter;
 		String heute;
-		String geburtstag;
-	
-	
-		System.out.println("Bitten geben sie das aktuelle Datum ein, in der Reinfolge: Tag, Monat, Jahr");
-		tag_h = scan.nextInt();
-		monat_h = scan.nextInt();
-		jahr_h = scan.nextInt();
+		String GeburtK;
 		
-		System.out.println("Bitte geben sie nun ihr Geburtsdatum ein, in der selben Reinfolge: ");
-		tag_g = scan.nextInt();
-		monat_g = scan.nextInt();
-		jahr_g = scan.nextInt();
-	
-		heute = tag_h + "." + monat_h + ".";
-		geburtstag = tag_g + "." + monat_g + ".";
-	
-		if(heute.equals(geburtstag)) {
-			System.out.println("Herzlichen Glueckwunsch zum Geburtstag");
+		Scanner scan = new Scanner(System.in);
+		
+		
+		System.out.println("Welcher Tag ist heute?");
+		Tag_heute = scan.nextInt();
+		System.out.println("Welcher Monat ist heute?");
+		Monat_heute = scan.nextInt();
+		System.out.println("Welches Jahr ist heute?");
+		Jahr_heute = scan.nextInt();
+		
+		System.out.println("An welchem Tag hast du Geburtstag?");
+		Geburtstag = scan.nextInt();
+		System.out.println("in welchem Monat hast du Geburtstag?");
+		Geburtsmonat = scan.nextInt();
+		System.out.println("In welchem Jahr hast du Geburtstag?");
+		Geburtsjahr = scan.nextInt();
+		scan.close();
+		
+		
+		if(Monat_heute < Geburtsmonat){
+			Jahr_heute = Jahr_heute-1;
+			alter = Jahr_heute - Geburtsjahr;
 		}
 		
-		alter = jahr_h - jahr_g;
+		else if(Monat_heute == Geburtsmonat) {
+			if(Tag_heute < Geburtstag) {
+				alter = --Jahr_heute - Geburtsjahr; 
+			}else {
+				alter = Jahr_heute - Geburtsjahr;
+			}
+		}else {
+			alter = Jahr_heute - Geburtsjahr;
+		}
+		
+		heute = Tag_heute + "." + Monat_heute;
+		GeburtK = Geburtstag + "." + Geburtsmonat;
+		if(heute.equals(GeburtK)) {
+			System.out.println("Alles gute zum Geburtstag");
+		}
+	
+		heute = Tag_heute + "." + Monat_heute + "." + Jahr_heute;
+		GeburtK = Geburtstag + "." + Geburtsmonat + "." + Geburtsjahr;
+		
 		System.out.println("Sie sind " + alter + " Jahre alt.");
+		System.out.println("Heute ist der " + heute);
+		System.out.println("Dein Geburtstag ist der " + GeburtK);
+	
+	
+	
+	
 	}
+
 }
