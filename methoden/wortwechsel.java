fa65921859a2c7797df6163038a48200da6a6a61
32mika <117844540+32mika@users.noreply.github.com>
2023-01-17 21:05:51 +0100

    Update wortwechsel.java

--- methoden/wortwechsel.java
+++ methoden/wortwechsel.java
@@ -2,47 +2,56 @@ package array;
 import java.util.Scanner;
 import java.util.Random;
 
-// Funktioniert noch nicht
 
 public class wortwechsel {
 
 	public static void main(String[] args) {
 		String wort;
 		String end;
+		String sonderzeichen;
 		char[] bst1;
 		char[] bst2;
  		int[] zahlen;
 		int count;
 		int zufallZ;
 		int i;
-		
-		//boolean doppelt;
-		
-		
+	
 		Scanner scan = new Scanner(System.in);
 		Random rand = new Random();
 		
 		while(scan.hasNext()) {
-			wort = scan.next(); // 7
-			bst1 = new char[wort.length()]; // arr größe 7
-			zahlen = new int[wort.length() - 2]; // arr größe 7
+			wort = scan.next(); 
+			bst1 = new char[wort.length()]; 
+			bst2 = new char[wort.length()];
+			zahlen = new int[wort.length() - 2];
 			
+			sonderzeichen = "";
 			count = 0;
 			i = 0;
 			bst1 = wort.toCharArray();
 			bst2 = wort.toCharArray();
 			
-			//System.out.print(bst1[0]);
+			System.out.print(bst1[0]);
 			
+			if(bst1[wort.length() -1] == 46 | bst1[wort.length() -1] == 44 | bst1[wort.length() -1] == 33 | bst1[wort.length() -1] == 63) {
+				sonderzeichen = Character.toString(bst1[wort.length()-1]);
+				wort = wort.substring(0, wort.length() -1);
+				
+				bst1 = new char[wort.length()]; 
+				bst2 = new char[wort.length()];
+				zahlen = new int[wort.length() -2];
+				bst1 = wort.toCharArray();
+				bst2 = wort.toCharArray();
 			
-			for(int j = 0; i < wort.length() -2; i++) {  // i bis 5
+			}
+			
+			for(int j = 0; i < wort.length() -2; i++) {  
 				zufallZ = rand.nextInt(wort.length()-2);
 				zufallZ++; 
 				zufallZ++;
 				count++;
-				
-				
-				for(int d = 0; d < count; d++){  // count 2
+			
+				for(int d = 0; d < count; d++){  
 					if(zahlen[d] == zufallZ){
 						i--;
 						count--;
@@ -51,25 +60,25 @@ public class wortwechsel {
 					}else if (d == count-1){
 						zahlen[count-1] = zufallZ;
 						break;
-					}
-					
-					
+					}	
 				}
-				
 			}
 			
-			for(int e = 1, c = 0; e < zahlen.length + 2; e++, c++) {
-				System.out.println(bst2[c] + "");
-				
-				//bst1[e] = bst2[zahlen[c]];
-				//System.out.print(bst1[e]);
-				
+			for(int e = 1, c = 0; c < zahlen.length; e++, c++) {
+				bst1[e] = bst2[zahlen[c]-1];
+				System.out.print(bst1[e]);
+					
 			}
-		
-		
-		}
-		
-		
+			
+			if(sonderzeichen != "") {
+				System.out.print(bst1[wort.length()-1]);
+				System.out.print(sonderzeichen + " ");
+				
+			}else {
+				System.out.print(bst1[wort.length()-1] + " ");
+			
+			}
+				
+		}	
 	}
-
 }
